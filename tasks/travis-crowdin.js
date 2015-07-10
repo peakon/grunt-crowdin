27214d13cdf43c3a7f64afd5d642f356262867f1
Christian Holm <holm@podio.com>
2015-07-10 15:15:27 +0200

    Add support for merging the source translations
    This way any new keys are not removed when downloading translations

--- tasks/travis-crowdin.js
+++ tasks/travis-crowdin.js
@@ -5,44 +5,77 @@ const path = require('path');
 const fs = require('fs');
 const unzip = require('unzip');
 const Promise = require('bluebird');
+const streamBuffers = require('stream-buffers');
+const _ = require('lodash');
+
+function upload(project, localesFolder, filename, sourceLocale) {
+	const options = {};
+	options['files[' + filename + '.json]'] = fs.createReadStream(path.join(localesFolder, sourceLocale + '.json'));
+
+	return api.updateFile(project, [], options);
+}
+
+function download(project, localesFolder, filename, sourceLocale) {
+	return api.exportTranslations(project).then(function() {
+		return Promise.fromNode(function(callback) {
+			const zipPipe = api.downloadAllTranslations(project).pipe(unzip.Parse());
+			zipPipe.on('entry', function(entry) {
+				if (entry.type === 'File') {
+					const splitPath = entry.path.split('/');
+					const locale = splitPath[0];
+					const name = splitPath[1];
+
+					if (name === filename + '.json') {
+						const targetFile = path.join(localesFolder, locale + '.json');
+
+						if (locale === sourceLocale) {
+							//We must merge the source translation with the updated translations
+							const localeBuffer = new streamBuffers.WritableStreamBuffer();
+
+							entry.pipe(localeBuffer).on('close', function() {
+								const updatedTranslations = JSON.parse(localeBuffer.getContentsAsString('utf8'));
+
+								fs.readFile(targetFile, 'utf8', function(err, data) {
+									if (err) return callback(err);
+
+									const mergedTranslations = _.merge(JSON.parse(data), updatedTranslations);
+
+									fs.writeFile(targetFile, JSON.stringify(mergedTranslations), 'utf8');
+								});
+							});
+						} else {
+							entry.pipe(fs.createWriteStream(path.join(localesFolder, locale + '.json')));
+						}
+					}
+				} else {
+					entry.autodrain();
+				}
+			}).on('close', callback).on('error', callback);
+		});
+	});
+}
 
 module.exports = function(grunt) {
 	grunt.registerTask('translations', function(target) {
-		var key = grunt.config([this.name, 'key']);
-		var project = grunt.config([this.name, 'project']);
-		var filename = grunt.config([this.name, 'filename']);
-		var folder = grunt.config([this.name, 'folder']);
-		var sourceLocale = grunt.config([this.name, 'sourceLocale']);
+		const key = grunt.config([this.name, 'key']);
+		const project = grunt.config([this.name, 'project']);
+		const filename = grunt.config([this.name, 'filename']);
+		const folder = grunt.config([this.name, 'folder']);
+		const sourceLocale = grunt.config([this.name, 'sourceLocale']);
 
 		api.setKey(key);
 
 		const localesFolder = path.join(process.cwd(), folder);
 
+		let taskPromise;
 		if (target === 'upload') {
-			const options = {};
-			options['files[' + filename + '.json]'] = fs.createReadStream(path.join(localesFolder, sourceLocale + '.json'));
-
-			api.updateFile(project, [], options).nodeify(this.async());
+			taskPromise = upload(project, localesFolder, filename, sourceLocale);
 		} else if (target === 'download') {
-			api.exportTranslations(project).then(function() {
-				return Promise.fromNode(function(callback) {
-					const zipPipe = api.downloadAllTranslations(project).pipe(unzip.Parse());
-					zipPipe.on('entry', function(entry) {
-						if (entry.type === 'File') {
-							const splitPath = entry.path.split('/')
-							const locale = splitPath[0];
-							const name = splitPath[1];
-							if (name === filename + '.json') {
-								entry.pipe(fs.createWriteStream(path.join(localesFolder, locale + '.json')));
-							}
-						} else {
-							entry.autodrain();
-						}
-					}).on('close', callback).on('error', callback);
-				});
-			}).nodeify(this.async());
+			taskPromise = download(project, localesFolder, filename, sourceLocale);
 		} else {
 			throw new Error('Unknown target \'' + target + '\', use \'upload\' or \'download\'.');
 		}
+
+		return taskPromise.nodeify(this.async());
 	});
 };
