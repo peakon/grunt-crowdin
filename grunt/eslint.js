b3f1abe69bda6fded3d6a15d521505b6cd261430
Christian Holm <cho@cubitech.dk>
2016-05-23 19:45:34 +0200

    Add the usual support stuff

---
+++ grunt/eslint.js
@@ -0,0 +1,5 @@
+module.exports = {
+  assets: {
+    src: ['**/*.js', '!node_modules/**/*']
+  }
+};
--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-module.exports = function(grunt) {
-	// Actually load this plugin's task(s).
-	grunt.loadTasks('tasks');
+module.exports = function (grunt) {
+  require('load-grunt-config')(grunt);
 };
