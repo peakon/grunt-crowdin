1de01002f9d6ac1c11f7edd3e3e463f68ca1ea7d
Christian Holm <cho@cubitech.dk>
2017-09-29 10:31:26 +0200

    Update to latest crowdin-api

--- tasks/translations.js
+++ tasks/translations.js
@@ -1,14 +1,14 @@
 'use strict';
 
-const api = require('crowdin-api');
+const CrowdinApi = require('crowdin-api');
 const path = require('path');
 const fs = require('fs');
 const unzip = require('unzip');
-const Promise = require('bluebird');
+const Bluebird = require('bluebird');
 const streamBuffers = require('stream-buffers');
 const _ = require('lodash');
 
-function upload(project, localesFolder, filename, sourceLocale) {
+function upload(api, project, localesFolder, filename, sourceLocale) {
   const options = {
     ['files[' + filename + '.json]']: fs.createReadStream(path.join(localesFolder, sourceLocale + '.json')),
     update_option: 'update_as_unapproved'
@@ -17,73 +17,81 @@ function upload(project, localesFolder, filename, sourceLocale) {
   return api.updateFile(project, [], options);
 }
 
-function download(project, localesFolder, filename, sourceLocale, mappings) {
-  return api.exportTranslations(project).then(function () {
-    return Promise.fromNode(function (callback) {
-      api.downloadAllTranslations(project).pipe(unzip.Parse())
-        .on('entry', function (entry) {
-          if (entry.type !== 'File') {
-            entry.autodrain();
-            return;
-          }
+async function download(api, project, localesFolder, filename, sourceLocale, mappings) {
+  await api.exportTranslations(project);
 
-          const [locale, ...rest] = entry.path.split('/');
-          const name = rest.join('/');
-          if (name !== filename + '.json') {
-            entry.autodrain();
-            return;
-          }
+  const zipPath = await api.downloadAllTranslations(project);
 
-          const targetFile = path.join(localesFolder, (mappings[locale] || locale) + '.json');
+  await new Bluebird((resolve, reject) => {
+    fs.createReadStream(zipPath)
+      .pipe(unzip.Parse())
+      .on('entry', async entry => {
+        if (entry.type !== 'File') {
+          return entry.autodrain();
+        }
 
-          if (locale === sourceLocale) {
-            // We must merge the source translation with the updated translations
-            const localeBuffer = new streamBuffers.WritableStreamBuffer();
+        const match = entry.path.match(/(.*)\/(.*)\.json/);
+        if (!match) {
+          throw new Error(`Unknown path ${entry.path}`);
+        }
+        const locale = mappings[match[1]] || match[1];
+        const projectFile = match[2];
+        if (projectFile !== filename) {
+          return entry.autodrain();
+        }
+        const targetFile = path.join(localesFolder, locale + '.json');
 
-            entry.on('end', function () {
-              const updatedTranslations = JSON.parse(localeBuffer.getContentsAsString('utf8'));
+        if (locale === sourceLocale) {
+          // We must merge the source translation with the updated translations
+          const buffer = new streamBuffers.WritableStreamBuffer();
+          const current = await Bluebird.fromCallback(cb => fs.readFile(targetFile, 'utf8', cb));
 
-              fs.readFile(targetFile, 'utf8', function (err, data) {
-                if (err) return callback(err);
+          entry.on('end', async function () {
+            const updated = buffer.getContentsAsString('utf8');
+            const merged = _.merge(JSON.parse(current), JSON.parse(updated));
 
-                const mergedTranslations = _.merge(JSON.parse(data), updatedTranslations);
-
-                fs.writeFile(targetFile, JSON.stringify(mergedTranslations, null, 4), 'utf8');
-              });
-            })
-              .pipe(localeBuffer);
-          } else {
-            entry.pipe(fs.createWriteStream(targetFile));
-          }
-        })
-        .on('close', callback)
-        .on('error', callback);
-    });
+            await Bluebird.fromCallback(cb => fs.writeFile(targetFile, JSON.stringify(merged, null, 4), 'utf8', cb));
+          })
+            .pipe(buffer);
+        } else {
+          entry.pipe(fs.createWriteStream(targetFile));
+        }
+      })
+      .on('close', resolve)
+      .on('error', reject);
   });
 }
 
 module.exports = function (grunt) {
-  grunt.registerTask('translations', function (target) {
-    const key = grunt.config([this.name, 'key']);
+  grunt.registerTask('translations', async function (target) {
+    const apiKey = grunt.config([this.name, 'key']);
     const project = grunt.config([this.name, 'project']);
     const filename = grunt.config([this.name, 'filename']);
     const folder = grunt.config([this.name, 'folder']);
     const sourceLocale = grunt.config([this.name, 'sourceLocale']);
     const mappings = grunt.config.getRaw([this.name, 'mappings']) || {};
 
-    api.setKey(key);
+    const api = new CrowdinApi({
+      apiKey
+    });
 
-    const localesFolder = path.join(process.cwd(), folder);
+    const done = this.async();
 
-    let taskPromise;
-    if (target === 'upload') {
-      taskPromise = upload(project, localesFolder, filename, sourceLocale);
-    } else if (target === 'download') {
-      taskPromise = download(project, localesFolder, filename, sourceLocale, mappings);
-    } else {
-      throw new Error('Unknown target \'' + target + '\', use \'upload\' or \'download\'.');
+    try {
+      const localesFolder = path.join(process.cwd(), folder);
+
+      let taskPromise;
+      if (target === 'upload') {
+        await upload(api, project, localesFolder, filename, sourceLocale);
+      } else if (target === 'download') {
+        await download(api, project, localesFolder, filename, sourceLocale, mappings);
+      } else {
+        throw new Error('Unknown target \'' + target + '\', use \'upload\' or \'download\'.');
+      }
+
+      done();
+    } catch (err) {
+      done(err);
     }
-
-    return taskPromise.nodeify(this.async());
   });
 };
